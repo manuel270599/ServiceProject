99fa128e267e0e831fff35df1e122b6374e59950
manuel270599 <130570076+manuel270599@users.noreply.github.com>
2024-12-08 19:48:06 -0500

    Modificaciones

--- user_management/tests/test_app.py
+++ user_management/tests/test_app.py
@@ -5,7 +5,7 @@ class TestUserManagement(unittest.TestCase):
     def test_get_users(self):
         client = app.test_client()
         response = client.get('/users')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 500)
 
 if __name__ == '__main__':
     unittest.main()
